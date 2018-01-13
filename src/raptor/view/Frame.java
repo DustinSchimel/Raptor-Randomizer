a5f2a3323cfa65fa0990196c8ced6e8e0a0ceb4d
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-12 19:55:10 -0700

    Completed frame and started panel

---
+++ src/raptor/view/Frame.java
@@ -0,0 +1,28 @@
+package raptor.view;
+
+import raptor.controller.Controller;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame
+{
+	private Controller appController;
+	private Panel appPanel;
+	
+	public Frame(Controller appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new Panel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("Raptor Randomizer");
+		this.setResizable(false);
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
+}
---
+++ src/raptor/view/Panel.java
@@ -0,0 +1,19 @@
+package raptor.view;
+
+import raptor.controller.Controller;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel extends JPanel
+{
+	private Controller appController;
+	private SpringLayout appLayout;
+	
+	public Panel(Controller appController)
+	{
+		
+	}
+
+}
