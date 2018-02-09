ebab4292457fb6dc18de3ae41825a059c91b8c8c
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-09 12:39:44 -0700

    Created an Aatrox class

---
+++ src/raptor/model/Aatrox.java
@@ -0,0 +1,14 @@
+package raptor.model;
+
+public class Aatrox extends Champion
+{
+	public Aatrox()
+	{
+		super("Aatrox", "The Darkin Blade");
+	}
+	
+	public Aatrox(String name)
+	{
+		super(name, "The Darkin Blade");
+	}
+}
