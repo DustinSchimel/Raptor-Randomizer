962294801b9a7a49e4912cc8d305e3942a12f493
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-23 12:19:25 -0700

    Imported the frame into the controller and created a champion list

--- src/raptor/controller/Controller.java
+++ src/raptor/controller/Controller.java
@@ -1,17 +1,45 @@
 package raptor.controller;
 
 import raptor.model.*;
-//import raptor.view.RaptorFrame;
+import raptor.view.Frame;
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class Controller
 {
+	private List<Champion> championList;
+	private Frame appFrame;
 	
 	public Controller()
 	{
+		championList = new ArrayList<Champion>();
+		buildChampionList();
 		
+		appFrame = new Frame(this);
+	}
+	
+	public void buildChampionList()
+	{
+		championList.add(new Aatrox());
+		championList.add(new Ahri());
+	}
+	
+	public List<Champion> getChampionList()
+	{
+		return championList;
+	}
+	
+	public String [] convertChampionList()
+	{
+		String [] names = new String [championList.size()];
+		
+		for (int index = 0; index < championList.size(); index++)
+		{
+			names[index] = championList.get(index).getName();
+		}
+		
+		return names;
 	}
 	
 	public void start()
